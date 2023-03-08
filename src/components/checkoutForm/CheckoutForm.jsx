3b3ce7e7f77953c2992d3ef1e53838c1aa7a3ab6
nayan2608 <nayanramani26@gmail.com>
2023-03-08 18:07:59 +0530

    change stripe payment route

--- src/components/checkoutForm/CheckoutForm.jsx
+++ src/components/checkoutForm/CheckoutForm.jsx
@@ -62,7 +62,7 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/success",
+        return_url: "https://fiverr-04yo.onrender.com//success",
       },
     });
 
